8d44e5b60e271458745d16736357ac584ea1ad03
zzqlplq <zzqlplq@gmail.com>
2020-06-22 19:10:49 +0800

    v-1 基础

--- calculator/src/App.tsx
+++ calculator/src/App.tsx
@@ -1,26 +1,134 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './style.scss'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function HeadTitle() {
+  return (<h1>Calculator </h1>)
 }
 
-export default App;
+
+class InputNum extends Component<any, any> {
+
+  render() {
+    return (
+      <input type="number" placeholder="input number" onChange={(e) => this.props.onChange(e)} />
+    )
+  }
+}
+
+interface CalculatorProps {
+  first: number,
+  second: number,
+  firstOnChange: any,
+  secondOnChange: any
+}
+
+
+class Calculator extends Component<CalculatorProps> {
+
+  handleFirstInput = (event: any) => {
+    this.props.firstOnChange(parseInt(event.target.value))
+  }
+
+  handleSecondInput = (event: any) => {
+    this.props.secondOnChange(parseInt(event.target.value))
+  }
+
+  render() {
+    return (<div>
+      <InputNum onChange={(event: any) => this.handleFirstInput(event)} />
+      <span> + </span>
+      <br />
+      <InputNum onChange={(event: any) => this.handleSecondInput(event)} />
+      <span> = </span>
+    </div>
+    )
+  }
+}
+
+interface FooterProps {
+  sum: number,
+  onClick: () => void
+}
+
+class Footer extends Component<FooterProps> {
+
+  render() {
+    return (
+      <div>
+        <button onClick={() => this.props.onClick()} > calculator </button>
+        <span> {this.props.sum} </span>
+      </div>
+    )
+  }
+}
+
+
+interface ContentProps {
+  first: number,
+  second: number,
+
+  tempFirst: number,
+  tempSecond: number,
+}
+
+class Content extends Component<any, ContentProps> {
+
+  constructor(props: ContentProps) {
+    super(props)
+    this.state = {
+      first: 0,
+      second: 0,
+      tempFirst: 0,
+      tempSecond: 0,
+    }
+  }
+
+  calculaor() {
+
+    console.log("calculaor")
+    this.setState({
+      first: this.state.tempFirst,
+      second: this.state.tempSecond
+    })
+  }
+
+  firstInputChange = (value: number) => {
+    this.setState({
+      tempFirst: value
+    })
+    console.log('this.first  ' + value)
+  }
+
+  secondInputChange = (value: number) => {
+    this.setState({
+      tempSecond: value
+    })
+    console.log('this.second:  ' + value)
+  }
+
+
+  render() {
+    return (
+      <div>
+        <HeadTitle />
+        <Calculator first={this.state.tempFirst}
+          second={this.state.tempSecond}
+          firstOnChange={(value: any) => this.firstInputChange(value)}
+          secondOnChange={(value: any) => this.secondInputChange(value)} />
+        <Footer onClick={() => this.calculaor()} sum={this.state.first + this.state.second} />
+      </div>
+
+    )
+  }
+}
+
+
+class App extends Component {
+  render() {
+    return (<Content />)
+  }
+}
+
+
+
+export default App
